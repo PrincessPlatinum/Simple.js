91620da1b0575fc70791c772fe5fa82c9afeb382
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-09-13 10:17:37 -0400

    Prettyfying

--- Simple.js
+++ Simple.js
@@ -5,35 +5,35 @@
 */
 
 function Sim(id) {
- 
-   var about = {
-      Version: 0.1,
-      Author: "Maria Williams",
-      Created: "Fall 2013",
-      Updated: "13 September 2013"
-   };
- 
-   if (id) {
- 
-      if (window === this) {
-         return new Sim(id);
-      }
 
-      this.e = document.getElementById(id);
-      return this;
-      return about;
-   }
+    var about = {
+        Version: 0.1,
+        Author: "Maria Williams",
+        Created: "Fall 2013",
+        Updated: "13 September 2013"
+    };
+
+    if (id) {
+
+        if (window === this) {
+            return new Sim(id);
+        }
+
+        this.e = document.getElementById(id);
+        return this;
+        return about;
+    }
 }
 
 Sim.prototype = {
-	button: function(text) {
-		var button = '<a class="wikia-button" id="' + text + '">' + text + '</a>';
-                this.e.innerHTML += button;
-		return this;
-        },
+    button: function (text) {
+        var button = '<a class="wikia-button" id="' + text + '">' + text + '</a>';
+        this.e.innerHTML += button;
+        return this;
+    },
 
-	addClick: function(handler) {
-		this.e.setAttribute("onclick", handler);
-		return this;
-	},
+    addClick: function (handler) {
+        this.e.setAttribute("onclick", handler);
+        return this;
+    },
 };
